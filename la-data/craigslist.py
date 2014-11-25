4aa18782b8e7a0385f3ab95d68544b2c79a500a5
Rafi Romero <rromero@fb.com>
2014-11-25 18:23:41 -0500

    2-space-indents; feel free to revert

--- la-data/craigslist.py
+++ la-data/craigslist.py
@@ -9,9 +9,9 @@ SOURCE = 'craigslist'
 
 MAX_PRICE = 4300
 NEIGHBORHOODS = [
-    Neighborhoods.UPPER_EAST_SIDE,
-    Neighborhoods.EAST_VILLAGE,
-    Neighborhoods.WEST_VILLAGE
+  Neighborhoods.UPPER_EAST_SIDE,
+  Neighborhoods.EAST_VILLAGE,
+  Neighborhoods.WEST_VILLAGE
 ]
 SEARCH_QUERY = 'doorman'
 
@@ -22,81 +22,81 @@ SECTION_MARKER = '<section id="postingbody">'
 SECTION_END = '</section'
 
 class CLDataLoader:
-    def __init__(self):
-        self._params = self._form_params()
-        self._browser = get_browser()
+  def __init__(self):
+    self._params = self._form_params()
+    self._browser = get_browser()
 
-    def load_data(self):
-        return self._load_data_fee(Fees.NO_FEE) + self._load_data_fee(Fees.FEE)
+  def load_data(self):
+    return self._load_data_fee(Fees.NO_FEE) + self._load_data_fee(Fees.FEE)
 
-    def _form_params(self):
-        params = map(lambda nh: 'nh=%d' % nh, NEIGHBORHOODS)
-        params.append('query=%s' % SEARCH_QUERY)
-        params.append('maxAsk=%d' % MAX_PRICE)
-        params.append('hasPic=1')
-        params.append('bedrooms=1')
-        return '&'.join(params)
+  def _form_params(self):
+    params = map(lambda nh: 'nh=%d' % nh, NEIGHBORHOODS)
+    params.append('query=%s' % SEARCH_QUERY)
+    params.append('maxAsk=%d' % MAX_PRICE)
+    params.append('hasPic=1')
+    params.append('bedrooms=1')
+    return '&'.join(params)
 
-    def _load_data(self, fee, url_part=None):
-        params = self._params
-        if url_part != None:
-            params = '%s&%s' % (url_part[url_part.find('?') + 1:], params)
-        url = CL_URL % (fee, params)
+  def _load_data(self, fee, url_part=None):
+    params = self._params
+    if url_part != None:
+      params = '%s&%s' % (url_part[url_part.find('?') + 1:], params)
+    url = CL_URL % (fee, params)
 
-        s = open_page(self._browser, url)
-        return self._format_data(fee, json.loads(s)[0])
+    s = open_page(self._browser, url)
+    return self._format_data(fee, json.loads(s)[0])
 
-    def _format_data(self, fee, list):
-        urls = []
-        listings = []
+  def _format_data(self, fee, list):
+    urls = []
+    listings = []
 
-        for item in list:
-            if 'NumPosts' in item:
-                urls.append(item['url'])
-                continue
+    for item in list:
+      if 'NumPosts' in item:
+        urls.append(item['url'])
+        continue
 
-            bedrooms = int(float(item['Bedrooms']))
-            if bedrooms != 1 or 'studio' in item['PostingTitle'].lower():
-                continue
+      bedrooms = int(float(item['Bedrooms']))
+      if bedrooms != 1 or 'studio' in item['PostingTitle'].lower():
+        continue
 
-            apartment = Apartment(
-                SOURCE,
-                item['PostingTitle'],
-                int(float(item['Ask'])),
-                BASE_URL + item['PostingURL'])
-            apartment.set_location(item['Latitude'], item['Longitude'])
-            apartment.set_posting_timestamp(item['PostedDate'])
-            apartment.set_has_fee(fee == Fees.FEE)
-            listings.append(apartment)
+      apartment = Apartment(
+        SOURCE,
+        item['PostingTitle'],
+        int(float(item['Ask'])),
+        BASE_URL + item['PostingURL'])
+      apartment.set_location(item['Latitude'], item['Longitude'])
+      apartment.set_posting_timestamp(item['PostedDate'])
+      apartment.set_has_fee(fee == Fees.FEE)
+      listings.append(apartment)
 
-        map(lambda listing: self._load_more_data(listing), listings)
-        return (listings, urls)
+    map(lambda listing: self._load_more_data(listing), listings)
+    return (listings, urls)
 
-    def _load_data_fee(self, fee):
-        (listings, urls) = self._load_data(fee)
+  def _load_data_fee(self, fee):
+    (listings, urls) = self._load_data(fee)
 
-        # Decluster any clusters returned
-        while len(urls) > 0:
-            next_urls = []
-            for url in urls:
-                (new_lists, new_urls) = self._load_data(fee, url)
-                listings += new_lists
-                next_urls += new_urls
-            urls = next_urls
+    # Decluster any clusters returned
+    while len(urls) > 0:
+      next_urls = []
+      for url in urls:
+        (new_lists, new_urls) = self._load_data(fee, url)
+        listings += new_lists
+        next_urls += new_urls
+      urls = next_urls
 
-        return listings
+    return listings
 
-    def _load_more_data(self, listing):
-        if listing.is_fully_loaded():
-            return
-        s = open_page(self._browser, listing.url)
+  def _load_more_data(self, listing):
+    if listing.is_fully_loaded():
+      return
+    s = open_page(self._browser, listing.url)
 
-        (section, s) = html_helper.find_in_between(s, SECTION_MARKER, SECTION_END)
-        section = html_helper.strip_tags(section)
-        listing.set_blurb(section)
-        listing.save_to_db()
+    (section, s) = html_helper.find_in_between(s, SECTION_MARKER, SECTION_END)
+    section = html_helper.strip_tags(section)
+    listing.set_blurb(section)
+    listing.save_to_db()
 
 if __name__ == '__main__':
-    listings = CLDataLoader().load_data()
-    print ' ----- GOT %d LISTINGS ----- ' % len(listings)
-    print "\n\n".join(map(lambda i: str(i), listings))
+  listings = CLDataLoader().load_data()
+  print ' ----- GOT %d LISTINGS ----- ' % len(listings)
+  print "\n\n".join(map(lambda i: str(i), listings))
--- la-data/runner.py
+++ la-data/runner.py
@@ -17,41 +17,39 @@ MAX_DIST_MILES = .3
 METERS_IN_MILE = 1609.344
 
 def main(data_source, output, distance):
-    if data_source == DATA_SOURCE_TEST:
-        listings = _get_test_listings()
-    else:
-        listings = _get_listings()
-        DBStore().save_transaction(listings)
+  if data_source == DATA_SOURCE_TEST:
+    listings = _get_test_listings()
+  else:
+    listings = _get_listings()
+    DBStore().save_transaction(listings)
 
-    meters_distance = METERS_IN_MILE * distance
-    listings = filter(lambda l: l.stop_distance < meters_distance, listings)
+  meters_distance = METERS_IN_MILE * distance
+  listings = filter(lambda l: l.stop_distance < meters_distance, listings)
 
-    if output == OUTPUT_DEBUG:
-        _print_out(listings)
-    else:
-        # TODO: Implement Real Output
-        pass
+  if output == OUTPUT_DEBUG:
+    _print_out(listings)
+  else:
+    # TODO: Implement Real Output
+    pass
 
 def _print_out(listings):
-    aggregated = _aggregate_by(listings,
-                               lambda l: l.stop['stop'],
-                               lambda l: (l.price,l.has_fee,l.title)) # for now, these 3 fields
+  aggregated = _aggregate_by(listings,
+                             lambda l: l.stop['stop'],
+                             lambda l: (l.price,l.has_fee,l.title)) # for now, these 3 fields
 
-    for stop in aggregated:
-        print '===== %s =====' % stop
+  for stop in aggregated:
+    print '===== %s =====' % stop
 
-        price_fee_title_s = aggregated[stop].keys()
-        price_fee_title_s.sort()
+    price_fee_title_s = aggregated[stop].keys()
+    price_fee_title_s.sort()
 
-        for price_fee_title in price_fee_title_s:
-            for listing in aggregated[stop][price_fee_title]:
-                if listing is aggregated[stop][price_fee_title][0]: # first
-                    print listing.get_str_lines()[0] # print first line (price+fee+title)
-                print listing.get_str_lines()[1] # print second line (url)
-            print
-        print '\n'
-            
-        #aggregated[stop].sort(key=lambda listing: listing.price)
+    for price_fee_title in price_fee_title_s:
+      for listing in aggregated[stop][price_fee_title]:
+        if listing is aggregated[stop][price_fee_title][0]: # first
+          print listing.get_str_lines()[0] # print first line (price+fee+title)
+        print listing.get_str_lines()[1] # print second line (url)
+      print
+    print '\n'
 
 def _aggregate_by(array, *fns):
   ret = {}
@@ -68,36 +66,36 @@ def _aggregate_by(array, *fns):
   return ret
     
 def _get_listings():
-    loaders = [
-        CLDataLoader(),
-        NYBitsLoader(),
-        RenthopLoader(),
-        ]
-    all_listings = map(lambda l: l.load_data(), loaders)
-    return reduce(lambda a, b: a + b, all_listings)
+  loaders = [
+    CLDataLoader(),
+    NYBitsLoader(),
+    RenthopLoader(),
+    ]
+  all_listings = map(lambda l: l.load_data(), loaders)
+  return reduce(lambda a, b: a + b, all_listings)
 
 def _get_test_listings():
-    return DBStore().get_all_stored_listings()
+  return DBStore().get_all_stored_listings()
 
 parser = argparse.ArgumentParser(description='Apartments stuff')
 parser.add_argument(
-    '--source',
-    dest='data_source',
-    default=DATA_SOURCE_TEST,
-    help='[test, real]'
-    )
+  '--source',
+  dest='data_source',
+  default=DATA_SOURCE_TEST,
+  help='[test, real]'
+  )
 parser.add_argument(
-    '--out',
-    dest='output',
-    default=OUTPUT_DEBUG,
-    help='[debug, csv]'
-    )
+  '--out',
+  dest='output',
+  default=OUTPUT_DEBUG,
+  help='[debug, csv]'
+  )
 parser.add_argument(
-    '--within',
-    dest='distance',
-    default=MAX_DIST_MILES,
-    help='Required distance from subway stop (miles)'
-    )
+  '--within',
+  dest='distance',
+  default=MAX_DIST_MILES,
+  help='Required distance from subway stop (miles)'
+  )
 
 args = parser.parse_args()
 main(args.data_source, args.output, float(args.distance))
